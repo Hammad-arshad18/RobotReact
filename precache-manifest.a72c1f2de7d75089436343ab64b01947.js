bd9538115720eacd2e4adf2dbd38fac2cf0dcfbc
Hammad-Arshad <hammad.arshad672@gmail.com>
2022-02-26 18:48:35 +0500

    Updates

---
+++ precache-manifest.a72c1f2de7d75089436343ab64b01947.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cfaa9037dc2541e9a572",
+    "url": "/RobotReact/static/css/main.375c2296.chunk.css"
+  },
+  {
+    "revision": "cfaa9037dc2541e9a572",
+    "url": "/RobotReact/static/js/main.cfaa9037.chunk.js"
+  },
+  {
+    "revision": "7ab2393a9ad8c2b58dcd",
+    "url": "/RobotReact/static/js/1.7ab2393a.chunk.js"
+  },
+  {
+    "revision": "bc39b086d8f25e838fc7",
+    "url": "/RobotReact/static/css/2.b856cbbf.chunk.css"
+  },
+  {
+    "revision": "bc39b086d8f25e838fc7",
+    "url": "/RobotReact/static/js/2.bc39b086.chunk.js"
+  },
+  {
+    "revision": "b79719b57a891e114132",
+    "url": "/RobotReact/static/js/runtime~main.b79719b5.js"
+  },
+  {
+    "revision": "33020e0017427de2da3f0f1933059d02",
+    "url": "/RobotReact/index.html"
+  }
+];
--- src/components/Scrool.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Scrool = (props) => {
-  return (
-    <div style={{ overflowY: "scroll", height: "600px" }}>{props.children}</div>
-  );
-};
-
-export default Scrool;
